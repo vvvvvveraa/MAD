aa7ae6dfea0532dae7df5281e181e8b66dc95319
vvvvvveraa <veralim.22@ichat.sp.edu.sg>
2025-02-06 17:19:28 +0800

    get the recycleview working with db but i cnt see the image

---
+++ app/src/main/java/com/sp/project13/EventAdapter.java
@@ -0,0 +1,63 @@
+package com.sp.project13;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class EventAdapter extends RecyclerView.Adapter<EventAdapter.EventViewHolder> {
+
+    private List<Event> eventList;
+
+    public EventAdapter(List<Event> eventList) {
+        this.eventList = eventList;
+    }
+
+    @NonNull
+    @Override
+    public EventViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_event, parent, false);
+        return new EventViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EventViewHolder holder, int position) {
+        Event event = eventList.get(position);
+        holder.eventTitle.setText(event.getTitle());
+        holder.eventNewDescription.setText(event.getNewDescription());
+
+        if (event.getImageUrl() != null && !event.getImageUrl().isEmpty()) {
+            Glide.with(holder.itemView.getContext())
+                    .load(event.getImageUrl())
+                    .into(holder.eventImage);
+        } else {
+            holder.eventImage.setImageResource(R.drawable.sky); // Ensure this resource exists
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return eventList.size();
+    }
+
+    public static class EventViewHolder extends RecyclerView.ViewHolder {
+        ImageView eventImage;
+        TextView eventTitle;
+        TextView eventNewDescription;
+
+        public EventViewHolder(@NonNull View itemView) {
+            super(itemView);
+            eventImage = itemView.findViewById(R.id.event_image);
+            eventTitle = itemView.findViewById(R.id.event_title);
+            eventNewDescription = itemView.findViewById(R.id.event_new_description);
+        }
+    }
+}
